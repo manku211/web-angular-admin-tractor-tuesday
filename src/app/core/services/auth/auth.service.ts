db2f81011824ee4d65a116133297b5d9142e974b
pravallika <pravallika@rapidinnovation.dev>
2024-04-10 11:07:16 +0530

    login intg and guards

---
+++ src/app/core/services/auth/auth.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  baseUrl = 'https://api-dev.tractortuesday.xyz/api/v1/';
+
+  constructor(private http: HttpClient) {
+    console.log(environment.API_URL);
+  }
+
+  adminLogin(credentials: any): Observable<any> {
+    return this.http.post<any>(this.baseUrl + 'admin/login', credentials);
+  }
+}
--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { SharedModule } from '../../../shared/shared.module';
+import { AuthService } from '../../../core/services/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [SharedModule, FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
   validateForm: FormGroup<{
@@ -17,15 +26,35 @@ export class LoginComponent {
   }> = this.fb.group({
     userName: ['', [Validators.required]],
     password: ['', [Validators.required]],
-    remember: [true]
+    remember: [true],
   });
 
-  constructor(private fb: NonNullableFormBuilder) {}
+  constructor(
+    private fb: NonNullableFormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {}
   submitForm(): void {
     if (this.validateForm.valid) {
       console.log('submit', this.validateForm.value);
+      let payload = {
+        email: this.validateForm.controls.userName.value,
+        password: this.validateForm.controls.password.value,
+      };
+      this.authService.adminLogin(payload).subscribe({
+        next: (data) => {
+          console.log(data);
+          if (data) {
+            localStorage.setItem('token', data?.data?.accessToken);
+            this.router.navigate(['/dashboard']);
+          }
+        },
+        error: (error) => {
+          console.error('An error occurred during admin login:', error);
+        },
+      });
     } else {
-      Object.values(this.validateForm.controls).forEach(control => {
+      Object.values(this.validateForm.controls).forEach((control) => {
         if (control.invalid) {
           control.markAsDirty();
           control.updateValueAndValidity({ onlySelf: true });
@@ -33,6 +62,4 @@ export class LoginComponent {
       });
     }
   }
-
-  
 }
