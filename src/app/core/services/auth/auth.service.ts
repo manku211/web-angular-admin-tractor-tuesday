9d171bee1fc3f9f3480a0a16c744d7fd20415fcf
pravallika <pravallika@rapidinnovation.dev>
2024-04-10 11:18:21 +0530

    temp env

--- src/app/core/services/auth/auth.service.ts
+++ src/app/core/services/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from '../../../../environments/environment';
+// import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -10,7 +10,7 @@ export class AuthService {
   baseUrl = 'https://api-dev.tractortuesday.xyz/api/v1/';
 
   constructor(private http: HttpClient) {
-    console.log(environment.API_URL);
+    // console.log(environment.API_URL);
   }
 
   adminLogin(credentials: any): Observable<any> {
